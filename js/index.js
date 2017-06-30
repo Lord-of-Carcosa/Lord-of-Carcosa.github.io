ba1fc2703d0d0e55b979cb45f708ed9915fdd7c6
Lord-of-Carcosa <jack.schutte@ssbbartgroup.com>
2017-06-30 13:36:13 -0700

    Added buttons

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function expand1() {
+    var x = document.getElementById('expandable1');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function expand2() {
+    var x = document.getElementById('expandable2');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
