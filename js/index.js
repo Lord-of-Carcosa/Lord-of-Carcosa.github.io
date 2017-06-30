22c63083782383ab55f4f4fc0a49dc29fc122711
Lord-of-Carcosa <jack.schutte@ssbbartgroup.com>
2017-06-30 13:46:49 -0700

    Attempted to fix javascript

--- js/index.js
+++ js/index.js
@@ -15,3 +15,39 @@ function expand2() {
         x.style.display = 'block';
     }
 }
+
+function expand3() {
+    var x = document.getElementById('expandable3');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function expand4() {
+    var x = document.getElementById('expandable4');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function expand5() {
+    var x = document.getElementById('expandable5');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function expand6() {
+    var x = document.getElementById('expandable6');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
