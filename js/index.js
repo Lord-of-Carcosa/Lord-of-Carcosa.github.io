7b89f9d8561084f27faa8755f30731dfdf3c5c87
Lord-of-Carcosa <jack.schutte@ssbbartgroup.com>
2017-07-28 11:24:03 -0700

    Attempted to fix issues with slider

--- js/index.js
+++ js/index.js
@@ -63,124 +63,44 @@ function showSlides(n) {
 function showWarning() {
   var danger = document.getElementById("dangerslider").value;
   if (danger <= 10) { 
-  document.getElementById("dangerwarning1").style.visibility = "visible";
-  document.getElementById("dangerwarning2").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+  document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "You probably should not be here. Leave now, traveler."
   }
   else if (10 < danger && danger <= 20) {
-    document.getElementById("dangerwarning2").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+  document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "Maybe you should just step in, buy some merchandise, and go home and say you did stuff."
   }
   else if (20 < danger && danger <= 30) {
-    document.getElementById("dangerwarning3").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning2").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+    document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "You might be able to venture beyond the gift shop, but not very far. Maybe check out the cities, and not the ancient ones; the modern ones. Suggestions: Arkham, Innsmouth."
   }
   else if (30 < danger && danger <= 40) {
-    document.getElementById("dangerwarning4").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning2").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+  document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "You're pretty brave. You can explore Arkham and Innsmouth's hidden secrets."
   }
   else if (40 < danger && danger <= 50) {
-    document.getElementById("dangerwarning5").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning2").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+ document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "Explore the depths of Arkham and Innsmouth, but take note also of Yuggoth."
   }
   else if (50 < danger && danger <= 60) {
-    document.getElementById("dangerwarning6").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning2").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+  document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "Most of the cities are probably a good choice to be explored, though don't get lost in Yuggoth or Hyperborea."
   }
   else if (60 < danger && danger <= 70) {
-    document.getElementById("dangerwarning7").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning2").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+   document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "Congratulations! You are sufficiently corageous enough that you have been selected for a special tour of Carcosa. Downloading directly into your brain right now should be a memory of events long past, including a tour through the palace of Carcosa! What an event!"
   }
   else if (70 < danger && danger <= 80) {
-    document.getElementById("dangerwarning8").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning2").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+  document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "Not content to explore Carcosa in your mind, you might want to try exploring it in real life!"
   }
   else if (80 < danger && danger <= 90) {
-    document.getElementById("dangerwarning9").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning2").style.visibility = "hidden";
- document.getElementById("dangerwarning10").style.visibility = "hidden";
+  document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "Visit the pristine landscape of R'lyeh, and visit the great Temple inside."
   }
   else {
-    document.getElementById("dangerwarning10").style.visibility = "visible";
-  document.getElementById("dangerwarning1").style.visibility = "hidden";
-  document.getElementById("dangerwarning3").style.visibility = "hidden";
-  document.getElementById("dangerwarning4").style.visibility = "hidden";
- document.getElementById("dangerwarning5").style.visibility = "hidden";
- document.getElementById("dangerwarning6").style.visibility = "hidden";
- document.getElementById("dangerwarning7").style.visibility = "hidden";
-  document.getElementById("dangerwarning8").style.visibility = "hidden";
- document.getElementById("dangerwarning9").style.visibility = "hidden";
- document.getElementById("dangerwarning2").style.visibility = "hidden";
+   document.getElementById("dangerwarning").style.visibility = "visible";
+ document.getElementById("dangerwarning").textContent = "Fight Cthulhu!"
   }
 }
 function handleBtnClick(event) {
