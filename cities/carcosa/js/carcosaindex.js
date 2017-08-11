f8d7d4054257eb5a0537fcb420178b56f182a4e5
Lord-of-Carcosa <jack.schutte@ssbbartgroup.com>
2017-08-11 10:01:19 -0700

    Added Carcosan content

---
+++ cities/carcosa/js/carcosaindex.js
@@ -0,0 +1,42 @@
+function expand1() {
+  var button = document.getElementById("myButton1");
+    var x = document.getElementById('expandable1');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } 
+  else {
+        x.style.display = 'block';
+    }
+   if (x.style.display === 'block') {
+    button.value = "Hide Information";
+  }
+  else {
+    button.value = "Show Information";
+  }
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
